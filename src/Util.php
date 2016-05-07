41bd27402300360eb6380771983d9b1b3eb94945
Johan Björk <johanimon@gmail.com>
2016-05-07 16:53:40 +0200

    Add convenience methods for extracting data

--- src/Util.php
+++ src/Util.php
@@ -16,20 +16,46 @@ class Util
      * @param ResponseInterface $response
      * @return array
      */
-    static function parseResponse(ResponseInterface $response): array
+    public static function parseResponse(ResponseInterface $response): array
     {
-        return json_decode((string) $response->getBody(), $assoc = true);
+        return json_decode((string)$response->getBody(), $assoc = true);
     }
 
     /**
      * @param ResponseInterface[] $responses
      * @return array
      */
-    static function parseResponses(array $responses) {
+    public static function parseResponses(array $responses): array
+    {
         $data = [];
         foreach ($responses as $response) {
             $d = Util::parseResponse($response);
             $data[] = $d;
+        }
+        return $data;
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @param string $dataKey
+     * @return mixed
+     */
+    public static function modelFromResponse(ResponseInterface $response, string $dataKey): array
+    {
+        $parsed = self::parseResponse($response);
+        return $parsed[$dataKey];
+    }
+
+    /**
+     * @param ResponseInterface[] $responses
+     * @param string $dataKey
+     * @return array
+     */
+    public static function modelsFromPaginatedResponse(array $responses, string $dataKey): array
+    {
+        $data = [];
+        foreach ($responses as $res) {
+            $data = array_merge($data, static::modelFromResponse($res, $dataKey));
         }
         return $data;
     }
